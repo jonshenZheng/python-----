df3479e4ca6ec1597d2fe9568e51f58af34bb9dc
jonshenZheng <835647369@qq.com>
2018-03-10 21:57:45 +0800

    11

--- downloadImg.py
+++ downloadImg.py
@@ -3,13 +3,15 @@ import urllib.request
 
 #tool
 
-import os
+import os,sys
 import random
 import time
 import math
 
 
 imgUrl = ['000.jpg','001.jpg','002.jpg','003.jpg','004.jpg']
+baseUrl = 'http://localhost/testimg/'
+saveUrl = 'images/'
 
 
 #获取url的文件名
@@ -22,7 +24,7 @@ def getfileName(url):
 #保存路径
 
 
-def downloadImg(imgList=[],pathUrl='',dirPath=''):
+def downloadImg(imgList=[],pathUrl='',dirPath='',randomNane=False):
 
     i = 0
     imglen = len(imgList)
@@ -31,11 +33,11 @@ def downloadImg(imgList=[],pathUrl='',dirPath=''):
 
         tname = imgList[x]
 
-        rTime = int(time.time)
+        rTime = int(time.time())
 
         rNumber = random.random()*100
 
-        ranName = 'img'+ str(rNumber0)+str(rTime)
+        ranName = 'img'+ str(rNumber)+str(rTime)
 
         if(tname):
             imgNmame = tname
@@ -44,6 +46,19 @@ def downloadImg(imgList=[],pathUrl='',dirPath=''):
 
         fullPath = pathUrl + imgNmame
 
+        #如果你在C:\test目录下执行python getpath\getpath.py，那么os.getcwd()会输出“C:\test”，sys.path[0]会输出“C:\test\getpath”
+
+        cwdPath = os.getcwd()
+
+        #检测存储图片目录存不存在
+        if not os.path.exists(dirPath):
+            #0755是什么意思不知道？  十进制为777 第一个7是所有者的权限。第二个是文件所属组的权限，第三个是其他人的权限。 r=4, w=2, x=1 代表 读、写、执行    
+            os.mkdir( dirPath )
+            
+        else:
+            #判断有没有读写权限
+            pass
+            
         response = urllib.request.urlopen(fullPath)
         cat_img = response.read()
 
@@ -55,4 +70,4 @@ def downloadImg(imgList=[],pathUrl='',dirPath=''):
             f.write(cat_img)
 
 
-downloadImg(imgUrl,'http://localhost/testimg/','images')
+downloadImg(imgUrl,baseUrl,saveUrl)
